78a5966ef9c422e41978cce944bc86b5778a77b3
Ege Demir <egesko19@gmail.com>
2022-05-09 16:41:18 -0500

    Final Updates

--- TestingwModel.m
+++ TestingwModel.m
@@ -1,17 +1,42 @@
 
+[y,Fs] = audioread('beep-02.wav');
+
+netX = importTensorFlowNetwork('model_X');
+netY = importTensorFlowNetwork('model_Y');
+netZ = importTensorFlowNetwork('model_Z');
+
 label = 0;
-data_Collected = double.empty;
+data_CollectedX = double.empty;
+data_CollectedY = double.empty;
+data_CollectedZ = double.empty;
+
 while true
     
     pause(0.06)
     currentSample = m.Acceleration;
-    data_Collected(end+1) = currentSample(3);
+    data_CollectedX(end+1) = currentSample(1);
+    data_CollectedY(end+1) = currentSample(2);
+    data_CollectedZ(end+1) = currentSample(3);
 
-    if(length(data_Collected) == 6)
-        label = classify(net,data_Collected);
-        data_Collected = double.empty;
+
+    if(length(data_CollectedX) == 10)
+
+        labelX = predict(netX,data_CollectedX);
+        labelY = predict(netY,data_CollectedY);
+        labelZ = predict(netY,data_CollectedZ);
+
+        data_CollectedX = double.empty;
+        data_CollectedY = double.empty;
+        data_CollectedZ = double.empty;
+
+        average = labelZ;
+
+        disp(average)
+        if(average>0.5)
+            sound(y,Fs)
+        end
     end
+    
 
-    disp(label)
 
 end
